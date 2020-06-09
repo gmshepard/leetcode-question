84dc604a172bf1470c9468d61338f32c629b2be1
gaomeng12 <gaomeng12@58.com>
2020-06-09 15:49:46 +0800

    add leetcode 94 BinaryTreeInorderTraversal

---
+++ src/main/java/com/gmshepard/leetcode/editor/cn/BinaryTreeInorderTraversal.java
@@ -0,0 +1,60 @@
+//Given a binary tree, return the inorder traversal of its nodes' values. 
+//
+// Example: 
+//
+// 
+//Input: [1,null,2,3]
+//   1
+//    \
+//     2
+//    /
+//   3
+//
+//Output: [1,3,2] 
+//
+// Follow up: Recursive solution is trivial, could you do it iteratively? 
+// Related Topics 栈 树 哈希表
+
+  
+package com.gmshepard.leetcode.editor.cn;
+
+import com.gmshepard.leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreeInorderTraversal{
+public static void main(String[] args) {
+    Solution solution = new BinaryTreeInorderTraversal().new Solution();
+}
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (current != null || !stack.isEmpty()) {
+            while (current != null ) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
